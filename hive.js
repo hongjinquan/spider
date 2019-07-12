8a9ebb9b82578f4fdda67a171133c01ce57dbb07
hongjinquan <hongjinquan@kaochong.com>
2019-07-12 16:20:47 +0800

    测试

---
+++ hive.js
@@ -0,0 +1,44 @@
+const jshs2 = require('jshs2');
+const { Configuration, IDLContainer, HiveConnection } = jshs2;
+const fs = require('fs');
+const config = JSON.parse(fs.readFileSync('./hive.config.json'));
+
+const options = {};
+
+options.auth = config[config.use].auth;
+options.host = config[config.use].host;
+options.port = config[config.use].port;
+options.timeout = config[config.use].timeout;
+options.username = config[config.use].username;
+options.hiveType = config[config.use].hiveType;
+options.hiveVer = config[config.use].hiveVer;
+options.cdhVer = config[config.use].cdhVer;
+options.thriftVer = config[config.use].thriftVer;
+options.maxRows = config[config.use].maxRows;
+options.nullStr = config[config.use].nullStr;
+options.i64ToString = config[config.use].i64ToString;
+
+
+const configuration = new Configuration(options);
+const idl = new IDLContainer();
+
+
+
+async function main() {
+  idl.initialize(configuration).then(async (rs) => {
+    console.log('=======', rs);
+    let connection = new HiveConnection(configuration, idl);
+    console.log('connection', connection);
+    let cursor = await connection.connect();
+    const data = await cursor.execute(config.Query.query);
+    console.log('data', data);
+  })
+  // let connection = new HiveConnection(configuration, idl);
+  // console.log('connection', connection);
+  // let cursor = await connection.connect();
+  // console.log('cursor', cursor);
+  // let serviceType = idl.ServiceType;
+  // console.log('serviceType', serviceType);
+}
+
+main();
